b7d6f0ad9c55e079b13137175d790e4c47b5ccb1
Hans Kristian Flaatten <hans.kristian.flaatten@dnt.no>
2016-07-21 11:13:40 +0200

    fix(api): attach status code to HttpError

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ router.get('/', (req, res) => {
 
 const requireAuth = (req, res, next) => {
   if (!req.headers['x-user-id']) {
-    next(new HttpError('X-User-Id header is required'), 401);
+    next(new HttpError('X-User-Id header is required', 401));
   } else {
     req.user = { id: parseInt(req.headers['x-user-id'], 10) };
     next();
