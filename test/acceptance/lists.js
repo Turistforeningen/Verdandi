966a5f7f80e70d5358e769ab17cee3bfecb3076d
Håvard Ranum <havard.ranum@me.com>
2016-08-26 12:06:29 +0200

    test(list): split endpoints in separate describe

--- test/acceptance/lists.js
+++ test/acceptance/lists.js
@@ -10,7 +10,7 @@ const users = require('../fixtures/dnt-users');
 
 const getUserData = auth.getUserData;
 
-describe.only('POST /lister/:liste/blimed', () => {
+describe('POST /lister/:liste/*', () => {
   before(() => {
     auth.getUserData = () => Promise.resolve(users[1]);
   });
@@ -23,36 +23,41 @@ describe.only('POST /lister/:liste/blimed', () => {
   // Append `/blimed` or `/meldav` to join or leave a list
   const url = `/api/dev/lister/${listId}`;
 
-  it('returns error for missing user auth', () => (
-    app.post(`${url}/blimed`)
-      .expect(401)
-      .expect({
-        code: 401,
-        message: 'X-User-Id header is required',
-      })
-  ));
 
-  it('adds the list id to the users lists array', done => {
-    app.post(`${url}/blimed`)
-      .set('X-User-Id', '1234')
-      .set('X-User-Token', 'abc123')
-      .expect(200)
-      .end((req, res) => {
-        assert.equal(res.body.message, 'Ok');
-        assert.notEqual(res.body.data.lister.indexOf(listId), -1);
-        done();
-      });
+  describe('POST /lister/:liste/blimed', () => {
+    it('returns error for missing user auth', () => (
+      app.post(`${url}/blimed`)
+        .expect(401)
+        .expect({
+          code: 401,
+          message: 'X-User-Id header is required',
+        })
+    ));
+
+    it('adds the list id to the users lists array', done => {
+      app.post(`${url}/blimed`)
+        .set('X-User-Id', '1234')
+        .set('X-User-Token', 'abc123')
+        .expect(200)
+        .end((req, res) => {
+          assert.equal(res.body.message, 'Ok');
+          assert.notEqual(res.body.data.lister.indexOf(listId), -1);
+          done();
+        });
+    });
   });
 
-  it('removes the list from the users lists array', done => {
-    app.post(`${url}/meldav`)
-      .set('X-User-Id', '1234')
-      .set('X-User-Token', 'abc123')
-      .expect(200)
-      .end((req, res) => {
-        assert.equal(res.body.message, 'Ok');
-        assert.equal(res.body.data.lister.indexOf(listId), -1);
-        done();
-      });
+  describe('POST /lister/:liste/meldav', () => {
+    it('removes the list from the users lists array', done => {
+      app.post(`${url}/meldav`)
+        .set('X-User-Id', '1234')
+        .set('X-User-Token', 'abc123')
+        .expect(200)
+        .end((req, res) => {
+          assert.equal(res.body.message, 'Ok');
+          assert.equal(res.body.data.lister.indexOf(listId), -1);
+          done();
+        });
+    });
   });
 });
