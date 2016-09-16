830a46e4c54d5160ae94d0814f01ab4259bbd1c8
Håvard Ranum <havard.ranum@me.com>
2016-09-16 11:11:52 +0200

    test(api): include checkin rules

--- test/acceptance/api.js
+++ test/acceptance/api.js
@@ -12,6 +12,9 @@ describe('GET /', () => {
     app.get(url)
       .expect(200)
       .expect(res => {
+        assert.equal(typeof res.body.checkin_new.rules, 'object');
+        assert.equal(typeof res.body.checkin_new.rules.max_distance, 'number');
+        assert.equal(typeof res.body.checkin_new.rules.timeout, 'number');
         Object.keys(res.body).forEach(key => {
           assert.equal(typeof res.body[key].url, 'string');
         });
